3533512c6f8acf9c07ff52a94527e207c3762c76
deliriumaeternum <eternalvaflya@gmail.com>
2024-08-06 19:14:28 +0300

    Add files via upload

---
+++ module_2_3.py
@@ -0,0 +1,20 @@
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+index = 0
+while index < len(my_list):
+	num = my_list[index]
+	index = index + 1
+	if num == 0:
+		continue
+	if num < 0:
+		break
+	print(num)
+
+
+
+
+
+
+
+
+
+
