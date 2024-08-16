ee4d0b255d64919accb947f58258ad42eb65f582
deliriumaeternum <eternalvaflya@gmail.com>
2024-08-16 20:28:46 +0300

    Update module_3_1.py

--- module_3_1.py
+++ module_3_1.py
@@ -1,33 +1,18 @@
 calls = 0
 def count_calls():
     global calls
-    calls += 1
-
-word = input('Введите слово: ')
-def string_info():
-    a = int(len(word))
-    b = word.upper()
-    c = word.lower()
-    info = (a, b, c)
+    calls+=1
+def string_info (string):
     count_calls()
-    return info
-info = string_info()
-print(info)
-
-word1 = input('Введите слово: ')
-list = input('Введите список: ')
-def is_contains():
-    string = word1.lower()
-    list_to_search = list
-    for i in range(len(list_to_search)):
-        if word1.lower() in list:
-            result = True
-            break
-        else:
-            result = False
-            continue
+    return (len(string), string.upper(), string.lower())
+def is_contains(string, list_to_search):
     count_calls()
-    return result
-result = is_contains()
-print(result)
+    string = string.lower()
+    list_to_search = [item.lower() for item in list_to_search]
+    return string in list_to_search
+
+print(string_info('Capybara'))
+print(string_info('Armageddon'))
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN'])) # Urban ~ urBAN
+print(is_contains('cycle', ['recycling', 'cyclic'])) # No matches
 print(calls)
