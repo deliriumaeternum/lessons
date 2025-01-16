596e4e4d766ed6c564d6c5366dad1fd171ebdfb2
deliriumaeternum <eternalvaflya@gmail.com>
2025-01-16 22:51:15 +0300

    Update module_16_4.py

--- module_16_4.py
+++ module_16_4.py
@@ -17,8 +17,8 @@ async def get_all_users() -> List[User]:
 
 @app.post("/user/{username}/{age}", response_model=User)
 async def create_user(
-        username: Annotated[str, Path(min_length=3, max_length=15, description='Enter username', example='UrbanUser')],
-        age: Annotated[int, Path(ge=14, le=100, description='Enter age', example='18')]) -> User:
+        username: Annotated[str, Path(min_length=3, max_length=15, description='Enter username')],
+        age: Annotated[int, Path(ge=14, le=100, description='Enter age')]) -> User:
     user_id = max((i.id for i in users), default=0) + 1
     user = User(id=user_id, username=username, age=age)
     users.append(user)
@@ -26,20 +26,21 @@ async def create_user(
 
 @app.put("/user/{user_id}/{username}/{age}", response_model=User)
 async def update_user(
-        user_id: Annotated[int, Path(min_length=1, max_length=15, description='Enter user_id', example='1')],
-        username: Annotated[str, Path(min_length=3, max_length=15, description='Enter username', example='UrbanUser')],
-        age: Annotated[int, Path(ge=14, le=100, description='Enter age', example='18')]) -> str:
+        user_id: Annotated[int, Path(min_length=1, max_length=15, description='Enter user_id')],
+        username: Annotated[str, Path(min_length=3, max_length=15, description='Enter username')],
+        age: Annotated[int, Path(ge=14, le=100, description='Enter age')]) -> User:
     for i in users:
         if i.id == user_id:
             i.username = username
             i.age = age
             return i
-    raise HTTPException(status_code=404, detail='User was not found')
+    else:
+        raise HTTPException(status_code=404, detail='Пользователя не существует')
 
 @app.delete("/user/{user_id}", response_model=User)
-async def delete_user(
-        user_id: Annotated[int, Path(min_length=1, max_length=15, description='Enter user_id', example='1')]) -> str:
+async def delete_user(user_id: int) -> str:
     for k, i in enumerate(users):
         if i.id == user_id:
             return users.pop(k)
-    raise HTTPException(status_code=404, detail='User was not found')
+    else:
+        raise HTTPException(status_code=404, detail='Пользователя не существует')
