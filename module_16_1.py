7c101e7792014324102fb00416bf1a73e76a5e15
deliriumaeternum <eternalvaflya@gmail.com>
2025-01-21 21:40:40 +0300

    Update module_16_1.py

--- module_16_1.py
+++ module_16_1.py
@@ -3,17 +3,17 @@ from fastapi import FastAPI
 app = FastAPI()
 
 @app.get("/")
-async def root() -> str:
-    return {"Главная страница"}
+async def welcome():
+    return {"message": "Главная сраница"}
 
 @app.get("/user/admin")
-async def admin() -> str:
-    return {"Вы вошли как администратор"}
+async def admin():
+    return {"message": "Вы вошли как администратор"}
 
 @app.get("/user/{user_id}")
-async def user_num(user_id: int) -> str:
-    return {f"Вы вошли как пользователь № {user_id}"}
+async def user_id(user_id: int):
+    return {"message": f"Вы вошли как пользователь №{user_id}"}
 
 @app.get("/user")
-async def user_info(username: str, age: int) -> str:
-    return {f"Информация о пользователе: имя - {username}, возраст - {age}"}
+async def user_paginator(username: str = 'Lada', age: int = 25):
+    return {"message": f"Информация о пользователе. Имя: {username}, Возраст: {age}"}
